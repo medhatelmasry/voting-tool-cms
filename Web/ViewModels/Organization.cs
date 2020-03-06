0a058a1b0a99ff314603af3f7cbed1ea7e5c8102
Dhillon-Prabh <psingh143105@gmail.com>
2020-03-06 12:17:03 -0800

    final commit

---
+++ Web/ViewModels/Organization.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Web.Models.Domain;
+
+namespace Web.ViewModels
+{
+    public class Organization
+    {
+        [Key]
+        [Display(Name = "OrganizationId")]
+        public int OrganizationId { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Candidates")]
+        public List<Candidate> Candidates { get; set; }
+    }
+}
